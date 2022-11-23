2e57d3b106b389549697d430ebea369425b4be9f
nicolas29200 <nicolas.quere3@gmail.com>
2022-11-23 15:57:26 +0100

    model.py

--- app/model/model.py
+++ app/model/model.py
@@ -2,8 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from xgboost import XGBRegressor
-
-datafull=pd.read_csv("../datasource/Wines.csv")
+datafull=pd.read_csv("app/datasource/Wines.csv")
 data=datafull.drop("Id",axis=1)
 data=data.drop_duplicates()
 
@@ -17,4 +16,4 @@ y = data["quality"]
 X_train, X_valid, y_train, y_valid = train_test_split(X, y,train_size=0.8, test_size=0.2,random_state=0)
 my_model = XGBRegressor(n_estimators=100,learning_rate=0.05)
 my_model.fit(X_train, y_train)
-my_model.save_model("modelXGBoost.json")
+my_model.save_model("app/model/modelXGBoost.json")
