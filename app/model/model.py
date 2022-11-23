92ce30b18a2e7cb6bdf3105318e05d6acf04f04d
nicolas29200 <nicolas.quere3@gmail.com>
2022-11-23 17:08:32 +0100

    change model.py

--- app/model/model.py
+++ app/model/model.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from csv import DictWriter
+from sklearn.metrics import mean_absolute_error
+
 
 def save_model():
     datafull=pd.read_csv("app/datasource/Wines.csv")
@@ -33,3 +35,21 @@ def add_data(row_dict):
     append_dict_as_row('app/datasource/Wines.csv', row_dict, field_names)
 
 #dicto={"fixed acidity":0,"volatile acidity":0,"citric acid":0,"residual sugar":0,"chlorides":0,"free sulfur dioxide":0,"total sulfur dioxide":0,"density":0,"pH":0,"sulphates":0,"alcohol":0,"quality":0,"Id":0}
+def description():
+    datafull=pd.read_csv("app/datasource/Wines.csv")
+    data=datafull.drop("Id",axis=1)
+    data=data.drop_duplicates()
+    #Select X
+    X=data[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
+        'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
+        'pH', 'sulphates', 'alcohol']]
+    # Select target
+    y = data["quality"]
+    # Separate data into training and validation sets
+    n_estimators=100
+    learning_rate=0.05
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y,train_size=0.8, test_size=0.2,random_state=0)
+    model_xgb= XGBRegressor(n_estimators,learning_rate)
+    model_xgb.load_model("app/model/modelXGBoost.json")
+    predictions = list(map(round,model_xgb.predict(X_valid)))
+    return {"n_estimators":n_estimators,"learning_rate":learning_rate,"mean_absolute_error":mean_absolute_error(predictions, y_valid)}
