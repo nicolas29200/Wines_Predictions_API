8fe8b92b3f60a136ec26463fb1e0fe089a5e1e41
nicolas29200 <nicolas.quere3@gmail.com>
2022-12-18 20:20:50 +0100

    docstring

--- app/model/model.py
+++ app/model/model.py
@@ -7,6 +7,11 @@ import numpy as np
 
 from app.model import read_add_csv
 def data_X_y():
+    """Sépare les données entre y qui contient la colonne qualité et X le reste
+
+    Returns:
+        _type_: Dataframe and Series
+    """    
     datafull=read_add_csv.read_CSV("app/datasource/Wines.csv")
     data=datafull.drop_duplicates()
 
@@ -18,9 +23,26 @@ def data_X_y():
     y = data["quality"]
     return X,y
 def Data_separation(X,y):
+    """Sépare les données entre données d'entrainement 80% et données de validation 20%
+
+
+    Args:
+        X (Dataframe): colonnes : 'fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
+        'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
+        'pH', 'sulphates', 'alcohol'
+        y (Series): Colonne qualité
+
+    Returns:
+        _type_: list
+    """    
+    
     X_train, X_valid, y_train, y_valid = model_selection.train_test_split(X, y,train_size=0.8, test_size=0.2,random_state=0)
     return [X_train, X_valid, y_train, y_valid]
 def save_model():
+    """_summary_
+    entraine le modèl et le sérialise au format json
+
+    """    
     X,y=data_X_y()
     # Separate data into training and validation sets
     Xy_Train_Test = Data_separation(X,y)
@@ -30,6 +52,14 @@ def save_model():
     my_model.save_model("app/model/modelXGBoost.json")
 
 def load_Model(my_model):
+    """ load le modèle sérialisé si il existe sinon serialise et load le modèle
+
+    Args:
+        my_model (Xgboost): Model
+
+    Returns:
+        _type_: renvoie le model
+    """    
     if os.path.isfile("app/model/modelXGBoost.json"):
         my_model.load_model("app/model/modelXGBoost.json")
 
@@ -39,6 +69,11 @@ def load_Model(my_model):
     return my_model
 
 def description():
+    """ Recuperer la description de notre model
+
+    Returns:
+        dictionnary : renvoie les caractérisque de notre model ainsi que sa mae
+    """    
     X,y=data_X_y()
     # Separate data into training and validation sets
     n_estimators=100
@@ -51,12 +86,25 @@ def description():
 
 
 def prediction(wine):
+    """Prédit la qualité d'un vin
+
+    Args:
+        wine (dictionnary): les données sur un vin
+
+    Returns:
+        int: renvoie la qualité du vin
+    """    
     my_model=XGBRegressor(n_estimators=100,learning_rate=0.05)
     my_model=load_Model(my_model)
     resultat=my_model.predict(wine)
     return resultat
 
 def BestWinesParameters():
+    """ Cherche les meilleurs parametres d'un vin en faisant le barycentre de tout ceux qui ont la meilleure note
+
+    Returns:
+        Array: caractéristiques
+    """    
     datafull=read_add_csv.read_CSV("app/datasource/Wines.csv")
     data=datafull.drop_duplicates()
     databis=data[data["quality"]==8][['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
--- app/test.py
+++ app/test.py
@@ -1,10 +1,14 @@
 import model.read_add_csv as read_add
 import pytest
 def test_read_CSV():
+    """Test la fonction Read_Csv
+    """    
     df=read_add.read_CSV("app/datasource/Wines.csv")
     assert(len(df.columns)==12)
 
 def test_add_data():
+    """Test la fonction add_data_tocsv
+    """    
     new_Wine={"fixed acidity":5,"volatile acidity":5,"citric acid":5,"residual sugar":5,"chlorides":5,"free sulfur dioxide":5,"total sulfur dioxide":5,"density":5,"pH":5,"sulphates":5,"alcohol":5,"quality":5,"Id":4}
 
     field_names=list(new_Wine.keys())
